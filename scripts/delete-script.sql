4697c1e780996a7ff1bc5ad1d547a70b192dd743
Masaru Ohashi Jr <masaru.junior@previc.gov.br>
2022-01-12 16:39:06 -0300

    delete-script atualizado

--- scripts/delete-script.sql
+++ scripts/delete-script.sql
@@ -1,12 +1,13 @@
-delete from divida_pi_2022.div_liberacao where seq_liberacao > 0;
-delete from divida_pi_2022.div_evento_tranche where seq_evento_contrato > 0;
-delete from divida_pi_2022.div_obrigacao where seq_obrigacao > 0;
-delete from divida_pi_2022.div_parametro_tranche where seq_tranche_contrato > 0;
-delete from divida_pi_2022.div_pagamento where seq_pagamento > 0;
-delete from divida_pi_2022.div_tranche_contrato where seq_tranche_contrato > 0;
-delete from divida_pi_2022.div_contrato where seq_contrato > 0;
-delete from divida_pi_2022.div_sistema_amortizacao where seq_sistema_amortizacao > 0;
-delete from divida_pi_2022.div_garantia_contra_garantia where seq_garantia > 0;
+delete from divida_pi_DEV.div_liberacao where seq_liberacao > 0;
+delete from divida_pi_DEV.div_pagamento_item where seq_evento_contrato > 0;
+delete from divida_pi_DEV.div_evento_tranche where seq_evento_contrato > 0;
+delete from divida_pi_DEV.div_obrigacao where seq_obrigacao > 0;
+delete from divida_pi_DEV.div_parametro_tranche where seq_tranche_contrato > 0;
+delete from divida_pi_DEV.div_pagamento where seq_pagamento > 0;
+delete from divida_pi_DEV.div_tranche_contrato where seq_tranche_contrato > 0;
+delete from divida_pi_DEV.div_contrato where seq_contrato > 0;
+delete from divida_pi_DEV.div_sistema_amortizacao where seq_sistema_amortizacao > 0;
+delete from divida_pi_DEV.div_garantia_contra_garantia where seq_garantia > 0;
 commit;
 
 
